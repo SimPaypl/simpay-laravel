6cf532fe5cc073e82285217ee9c97cff310b152e
Patryk Vizauer <vizauerp@gmail.com>
2025-12-25 10:30:21 +0100

    chore: bump version

--- src/SimPay.php
+++ src/SimPay.php
@@ -8,7 +8,7 @@ use SimPay\Laravel\Services\Sms\Sms;
 
 class SimPay
 {
-    public const string VERSION = '1.4.1';
+    public const string VERSION = '1.4.2';
 
     public function payment(): Payment
     {
